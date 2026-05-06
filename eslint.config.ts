8339802ea4eefd8568fe636f66c5b9091a343052
Elliot Wong <elly0t.w@gmail.com>
2026-05-06 11:20:02 +0800

    chore: set up development formatting, linting and prettier

---
+++ eslint.config.ts
@@ -0,0 +1,17 @@
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default tseslint.config(
+  { ignores: ['dist', 'coverage', 'node_modules'] },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+    plugins: { 'react-hooks': reactHooks },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
+);
