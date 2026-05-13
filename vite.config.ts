855753a331662ed885a36781440773a60f5121f6
Elliot Wong <elly0t.w@gmail.com>
2026-05-13 16:59:46 +0800

    fix: strip debug lines in prod build in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: { port: 2048 },
-  esbuild: { drop: ['console', 'debugger'] },
+  esbuild: mode === 'production' ? { drop: ['console', 'debugger'] } : {},
   test: {
     exclude: ['e2e/**', 'node_modules/**', 'dist/**'],
   },
-});
+}));
